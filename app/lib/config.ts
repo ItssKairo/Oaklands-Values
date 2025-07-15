5b9bcecb9e98fd74af592085cc6b5fde3b1f2a6f
ItzKoryx <cormacjustinmccarthy@gmail.com>
2025-07-15 13:48:57 +0930

    fix: Attempt to fix build error

---
+++ app/lib/config.ts
@@ -0,0 +1,5 @@
+export const AppConfig = {
+  apiAvailable: false,
+  stockApiUrl: 'https://public-api.typicaldevelopers.com/v1/oaklands/economy/stock-market',
+  leaderboardApiUrl: 'https://public-api.typicaldevelopers.com/v1/oaklands/leaderboards/top-materials-today?currencyType=Cash'
+};
